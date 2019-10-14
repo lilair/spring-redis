3667de5f93a6cc795c7f87f520a861d57043bac0
李成树 <1130270216@qq.com>
2019-10-14 10:42:06 +0800

    第二次提交

--- src/test/java/com/changhong/spring/SpringRedisApplicationTests.java
+++ src/test/java/com/changhong/spring/SpringRedisApplicationTests.java
@@ -6,13 +6,33 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringRedisApplicationTests {
     @Autowired
     DepartmentMapper departmentMapper;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    RedisTemplate redisTemplate;
+    @Test
+    public void test01(){
+        redisTemplate.opsForSet().add("list",1);
+        redisTemplate.opsForSet().add("list",2);
+        Set members = redisTemplate.opsForSet().members("list");
+        System.out.println(members);
+        redisTemplate.opsForValue().set("name","lilair");
+        Object name = redisTemplate.opsForValue().get("str");
+        System.out.println(name);
+    }
+
     @Test
     public void contextLoads() {
         Department department = departmentMapper.findById(1);
